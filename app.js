6c789ba452245812bbaa95969fa4bd0d964e92b6
Muzammil Hussain <pm2498233@gmail.com>
2024-02-29 11:10:00 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,83 @@
+
+
+// Chapter 01
+
+// Q1
+
+
+// var abc = "Error! Please inter a valid password";
+
+// alert(abc)
+
+
+// Q2
+
+// var abc = "Wellcome to JS Land... \n Happy Coading"
+
+// alert(abc)
+
+// Q3
+
+// var abc = "Hello... I can run JS through my web browser's console"
+
+// alert(abc)
+
+
+// Chapter 02 
+
+// Q1
+
+// var abc = "Muzammil Hussain"
+
+// alert(abc)
+
+// Q2 
+
+// var abc = "15 years old"
+
+// alert(abc)
+
+// Q3
+
+// var abc = "Certified Mobile Application Development"
+
+// alert(abc)
+
+// Q4
+
+// var abc = "PIZZA\nPIZ\nPIZ\nPI\nP"
+
+// alert(abc)
+
+// Q5
+
+// var abc = "My Email Adrss Is Muzammil@Hussain.com"
+
+// alert(abc)
+
+// Q6 
+
+// var abc = "I am trying to Learn from The Booo A smarter\nway to learn JavaScript"
+
+// alert(abc)
+
+// Q7
+
+// var abc = "⫷M⫸⫷U⫸⫷Z⫸⫷A⫸⫷M⫸⫷M⫸ ⫷I⫸⫷L⫸⫷..⫸⫷..⫸⫷..⫸"
+
+// alert(abc)
+
+
+// Chapter 03
+
+// Q1
+
+// var abc = "I am 18 Years Old"
+
+// alert(abc)
+
+// Q2
+
+var abc = "The End "
+
+alert(abc)
